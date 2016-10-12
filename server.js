bca3ef3dabca0bbe802de23acb634b04d985b09e
jyothindra <jyothindrapavan@gmail.com>
2016-10-12 09:15:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,8 +8,14 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-1', function(req,res) {
+app.get('/article-one', function(req,res) {
     res.send('article one is requested');
+});
+app.get('/article-two', function(req,res) {
+    res.send('article two is requested');
+});
+app.get('/article-three', function(req,res) {
+    res.send('article three is requested');
 });
 
 app.get('/ui/style.css', function (req, res) {
