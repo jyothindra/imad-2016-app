b34cb42555a3a1ebf5aedd90a35b1ce4bd58f411
jyothindra <jyothindrapavan@gmail.com>
2016-10-15 08:54:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
 console.log('Loaded!');
 // change text of of the inner html
-var element = getElementById("main-text");
-element.innerHTML = "new value";
+var element = getElementById('main-text');
+element.innerHTML = 'new value';
