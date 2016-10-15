8f76d67378b339e7a5ae411a0b50207414cfe37a
jyothindra <jyothindrapavan@gmail.com>
2016-10-15 09:07:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,5 +6,5 @@ element.innerHTML = 'new value';
 // move the image
 var img = document.getElementById('madi');
 img.onclick = function(){
-  var interval = setInterval(moveLeft,  100); 
+  var interval = setInterval(moveRight,  100); 
 };
