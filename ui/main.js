80b605af725292691eac92652ca193de9e7b62fc
jyothindra <jyothindrapavan@gmail.com>
2016-10-15 09:01:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 // change text of of the inner html
 var element = document.getElementById('main-text');
 element.innerHTML = 'new value';
+
+// move the image
+var img = document.getElementById('madi');
+madi.onclick = function(){
+  img.style.marginLeft = '100px';  
+};
